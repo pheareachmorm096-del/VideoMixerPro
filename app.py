69b4a06f718e0800fd997836ce859344766f97c3
GOODHILL\pheareach.morm <pheareachmorm04@gmail.com>
2026-02-06 08:49:54 +0700

    update production

--- app.py
+++ app.py
@@ -217,5 +217,8 @@ def import_freesound():
         return jsonify({"error": "Failed"}), 400
     except Exception as e: return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, threaded=True)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000, threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
